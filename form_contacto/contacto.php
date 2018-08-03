f9e7bc03a5d150416a84fa685b4b3d037e00bac8
huggut <huggutg@gmail.com>
2018-08-03 16:25:57 -0500

    Formulario contacto

---
+++ form_contacto/contacto.php
@@ -0,0 +1,26 @@
+<?php
+
+	$nombre = $_POST["nombre"];
+	$email = $_POST["email"];
+	$asunto = $_POST["asunto"];
+	$mensaje = $_POST["mensaje"];
+
+	$para = "huggut@hotmail.com";
+	$asunto1 = "Prueba PHP $asunto $nombre";
+
+	$mensaje = "
+		Nombre del remitente: ".$nombre."
+		correo: ".$email."
+		asunto: ".$asunto."
+		mensaje: ".$mensaje."
+	";
+
+	if(mail($para,$asunto1,utf8_decode($mensaje))){
+		echo "$nombre " . "Gracias por enviarnos tus comentarios";
+	}else{
+		echo "El mensaje no pudo ser enviado, por favor intente más tarde";
+	}
+
+//  header ("location: index.html");
+
+ ?>
